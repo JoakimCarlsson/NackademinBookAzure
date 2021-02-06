45db5572170606b3b934d3f60f8faa841d766541
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2021-02-06 08:07:11 +0100

    Add project files.

---
+++ NackademinBookAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NackademinBookAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
